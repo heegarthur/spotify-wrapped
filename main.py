e07575aacdd554aaa636ceaaf4e84938e67221a2
ivo <ivoheeg@gmail.com>
2024-12-26 16:42:15 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,4 @@
+client_id='d871746f613a4a29ace9148a3ffb81a8',
+                                               client_secret='8141e6aa48134f808430ae9d2323986c',
+                                               redirect_uri='http://localhost:8888/callback',
+                                               scope='user-read-recently-played'))
