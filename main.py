eb9505e37be5fffbb0a8c295ca45c5135eb2ee27
ivo <ivoheeg@gmail.com>
2024-12-26 16:51:50 +0100

    Delete main.py

--- main.py
+++
@@ -1,4 +0,0 @@
-client_id='d871746f613a4a29ace9148a3ffb81a8',
-                                               client_secret='8141e6aa48134f808430ae9d2323986c',
-                                               redirect_uri='http://localhost:8888/callback',
-                                               scope='user-read-recently-played'))
